7af98d4b6115f2717edd758c2cdf742d47ad6f8d
Gaël <glanelepine@etu.cvm.qc.ca>
2020-11-19 15:33:05 -0500

    echo_01 terminé

--- 01-hello_world.sh
+++ 01-hello_world.sh
@@ -1,2 +1,20 @@
 #!/bin/bash
 
+# ceci est un commentaire
+
+echo "salut Gaël"
+# ou
+echo salut Gaël
+# ou
+echo 'salut Gaël'
+
+echo -n "Vive "
+echo "Linux"
+# va écrire Vive Linux sans saut de ligne
+
+echo -e "1\n2\n3\n4" # \n -e interprète les métacaractères
+
+# read = cin, -p "mon message" >> variable
+read -p "My name is? " nom 
+
+echo "tchic ka tchick la tchic" $nom

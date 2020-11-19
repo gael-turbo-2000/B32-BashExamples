6038750565cfa7774203de6f02ddfdb14d7df680
Gaël <glanelepine@etu.cvm.qc.ca>
2020-11-19 16:30:13 -0500

    calculs

--- 03-calculs.sh
+++ 03-calculs.sh
@@ -1,2 +1,14 @@
 #!/bin/bash
 
+echo 10+10 # affiche 10+10 (en texte)
+echo $((10+10)) # additionne des nombres
+
+let resultat=10+10 #si on écrit let devant la variable qu'on assigne, agit comme un $(())
+echo $resultat
+
+#soustraire 2e paramètre du 1er valeurs passées en paramètre
+let resultat=$1-$2
+echo $resultat
+# OU
+echo "le résultat de $1 - $2 = $(($1-$2))"
+echo "Allô le signe de piasses \$"
